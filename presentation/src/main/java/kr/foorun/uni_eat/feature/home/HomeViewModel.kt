a9c4706c7c1724fee58fd68bc430be76b0a5d802
jaden <jin7011>
2023-03-08 16:56:22 +0900

    add homeView

---
+++ presentation/src/main/java/kr/foorun/uni_eat/feature/home/HomeViewModel.kt
@@ -0,0 +1,10 @@
+package kr.foorun.uni_eat.feature.home
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kr.foorun.uni_eat.base.viewmodel.BaseViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+): BaseViewModel() {
+}
