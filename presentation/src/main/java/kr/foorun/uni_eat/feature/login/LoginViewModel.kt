2879e0252c44c02d10de56ccb12b0ee726870e32
jaden <jin7011>
2023-03-15 17:10:27 +0900

    add kakaoLogin

--- presentation/src/main/java/kr/foorun/uni_eat/feature/login/LoginViewModel.kt
+++ presentation/src/main/java/kr/foorun/uni_eat/feature/login/LoginViewModel.kt
@@ -1,8 +1,10 @@
 package kr.foorun.uni_eat.feature.login
 
+import android.util.Log
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import kr.foorun.social_login.KakaoLoginModule
 import kr.foorun.uni_eat.base.viewmodel.BaseViewModel
 import kr.foorun.uni_eat.base.viewmodel.MutableEventFlow
 import kr.foorun.uni_eat.base.viewmodel.asEventFlow
@@ -14,10 +16,19 @@ class LoginViewModel @Inject constructor(): BaseViewModel() {
     private val _eventFlow = MutableEventFlow<LoginEvent>()
     val eventFlow = _eventFlow.asEventFlow()
 
+    private val _kakaoLoginCallback = object : KakaoLoginModule.KakaoLoginCallback {
+        override fun onSuccess(accessToken: String, email: String?) { event(LoginEvent.KakaoSuccess(accessToken, email)) }
+        override fun onFailure(error: Throwable?) { event(LoginEvent.KakaoFailure(error)) }
+    }
+    val kakaoLoginCallback: KakaoLoginModule.KakaoLoginCallback
+        get() = _kakaoLoginCallback
+
     fun event(event: LoginEvent) = viewModelScope.launch { _eventFlow.emit(event) }
 
     sealed class LoginEvent{
         data class KakaoLogin(val unit: Unit? = null): LoginEvent()
+        data class KakaoSuccess(val accessToken: String, val email: String?): LoginEvent()
+        data class KakaoFailure(val error: Throwable? = null): LoginEvent()
     }
 
     fun clickedKakaoLogin() = event(LoginEvent.KakaoLogin())
