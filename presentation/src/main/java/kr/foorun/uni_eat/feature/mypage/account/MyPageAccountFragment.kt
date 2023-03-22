e9c110ad0d4d4340780c92b4e9a3a6229250a31b
jaden <jin7011>
2023-03-22 14:28:22 +0900

    add kakao logout and change navigation flow

---
+++ presentation/src/main/java/kr/foorun/uni_eat/feature/mypage/account/MyPageAccountFragment.kt
@@ -0,0 +1,40 @@
+package kr.foorun.uni_eat.feature.mypage.account
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import dagger.hilt.android.AndroidEntryPoint
+import kr.foorun.presentation.databinding.FragmentMyPageAccountBinding
+import kr.foorun.social_login.KakaoLoginClass
+import kr.foorun.uni_eat.base.view.base.context_view.BaseFragment
+import kr.foorun.uni_eat.base.viewmodel.repeatOnStarted
+import kr.foorun.uni_eat.feature.splash.SplashFragmentDirections
+
+@AndroidEntryPoint
+class MyPageAccountFragment :BaseFragment<FragmentMyPageAccountBinding,MyPageAccountViewModel>(FragmentMyPageAccountBinding::inflate){
+    override val fragmentViewModel: MyPageAccountViewModel by viewModels()
+
+    override fun observeAndInitViewModel() = binding {
+        viewModel = fragmentViewModel.apply {
+            repeatOnStarted { eventFlow.collect{ handleEvent(it) } }
+            repeatOnStarted { viewEvent.collect{ popUpBackStack() } }
+        }
+    }
+
+    private val kakaoLoginClass by lazy { KakaoLoginClass(requireContext(), fragmentViewModel.kakaoLoginCallback) }
+
+    private fun handleEvent(event: MyPageAccountViewModel.AccountEvent) = when(event){
+        is MyPageAccountViewModel.AccountEvent.ChangePassClicked -> {}
+        is MyPageAccountViewModel.AccountEvent.LogoutClicked -> kakaoLoginClass.logout()
+        is MyPageAccountViewModel.AccountEvent.WithdrawClicked -> {}
+        is MyPageAccountViewModel.AccountEvent.LogoutFailure -> {}
+        is MyPageAccountViewModel.AccountEvent.LogoutSuccess ->
+            navigateToFrag(SplashFragmentDirections.actionToLoginFragment())
+    }
+
+    override fun afterBinding(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) = binding{
+
+    }
+
+}
